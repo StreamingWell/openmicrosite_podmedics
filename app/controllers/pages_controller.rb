240f32848dd4d1f613dc5ccafbced74aeef9ac39
Branden Mittra <branden.mittra@streamingwell.com>
2014-09-29 10:27:48 +0100

    Disable temp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   # remove this before deploying to production
-  # before_filter :authenticate
+  before_filter :authenticate
 
   def home
   end
@@ -33,10 +33,10 @@ class PagesController < ApplicationController
 
   #protected
 
-   # def authenticate
-  #	  authenticate_or_request_with_http_basic do |username, password|
-  #	    username == "admin" && password == "prostate2013"
-   #	  end
-   # end
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+    username == "admin" && password == "prostate2013"
+   	end
+  end
 
 end
